d0974c98e05d00a940cb6a2e38f39c35379a68a8
AliAyub007 <akhan.bscs15seecs@seecs.edu.pk>
2017-08-02 08:11:59 +0500

    Updated deploy.sh

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -3,10 +3,14 @@ export DIR=${BASH_SOURCE%/*}
 
 if [ "$1" = "delete" ]; then
     echo "Clearing the cluster."
-    kubectl delete -f ${DIR}/yamls/generator
+    kubectl delete -f ${DIR}/yamls/nginx
+    kubectl delete -f ${DIR}/yamls/echoserver
+    kubectl delete -f ${DIR}/yamls/lego
     echo "Done. The project was removed from the cluster."
 elif [ "$1" = "create" ]; then
     echo "Deploying the project to kubernetes cluster"
-    kubectl create -f ${DIR}/yamls/generator
+    kubectl create -f ${DIR}/yamls/nginx
+    kubectl create -f ${DIR}/yamls/echoserver
+    kubectl create -f ${DIR}/yamls/lego
     echo "Done. The project was deployed to kubernetes. :)"
 fi
