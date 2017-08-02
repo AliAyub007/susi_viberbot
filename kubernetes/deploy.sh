c29d765310d7035b94bae5436ed1cbe36669c518
AliAyub007 <akhan.bscs15seecs@seecs.edu.pk>
2017-08-02 09:04:05 +0500

    test

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,16 +1,22 @@
-#!/bin/bash
 export DIR=${BASH_SOURCE%/*}
 
 if [ "$1" = "delete" ]; then
     echo "Clearing the cluster."
-    kubectl delete -f ${DIR}/yamls/nginx
-    kubectl delete -f ${DIR}/yamls/echoserver
-    kubectl delete -f ${DIR}/yamls/lego
+    if [ "$2" = "all" ]; then
+        kubectl delete -f ${DIR}/yamls/lego/00-namespace.yml
+        kubectl delete -f ${DIR}/yamls/nginx/00-namespace.yml
+    fi
     echo "Done. The project was removed from the cluster."
 elif [ "$1" = "create" ]; then
     echo "Deploying the project to kubernetes cluster"
-    kubectl create -f ${DIR}/yamls/nginx
-    kubectl create -f ${DIR}/yamls/echoserver
-    kubectl create -f ${DIR}/yamls/lego
+    if [ "$2" = "all" ]; then
+        # Start KubeLego deployment
+        kubectl create -R -f ${DIR}/yamls/lego
+        # Start nginx deployment, ingress & service
+        kubectl create -R -f ${DIR}/yamls/nginx
+    fi
+    kubectl create -R -f ${DIR}/yamls/application
+    echo "Waiting for server to start up. ~30s."
+    sleep 30
     echo "Done. The project was deployed to kubernetes. :)"
 fi
